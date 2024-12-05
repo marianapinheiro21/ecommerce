d5a08a880b7364135f0e4aface80647b22a82a97
GustavoFigueiredoMartins <gf-martins@hotmail.com>
2024-12-05 01:15:33 +0000

    Update urls.py
    Logista_dados em urls.py

--- Django/Proj_Ecommerce/projpsi/urls.py
+++ Django/Proj_Ecommerce/projpsi/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cliente/', views.clientes, name='cliente'),
     path('logista/', views.logista, name='logista'),
+    path('logista_dados/', views.logista_dados, name='logista_dados'),
     path('carrinho/', views.carrinho, name='carrinho'),
     path('produto/', views.produto, name='produto'),
     path('lista/', views.my_view, name='lista'),
