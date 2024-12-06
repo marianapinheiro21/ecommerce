ddd01df3653858cb02b0b8f7fc2271affe6467dc
Natany <natanymarianopt@hotmail.com>
2024-12-06 22:40:20 +0000

    Criar uma página para cada categoria de produtos

--- Django/Proj_Ecommerce/projpsi/urls.py
+++ Django/Proj_Ecommerce/projpsi/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('logista/', views.logista, name='logista'),
     path('carrinho/', views.carrinho, name='carrinho'),
     path('produto/', views.produto, name='produto'),
+    path('produto/computadores/', views.produtos_computadores, name='produtos_computadores'),
+    path('produto/acessorios/', views.produtos_acessorios, name='produtos_acessorios'),
+    path('produto/portateis/', views.produtos_portateis, name='produtos_portateis'),
     path('lista/', views.my_view, name='lista'),
 ]
